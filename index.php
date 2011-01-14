159e84e7e0b1886823c7a6dc6625b3ed6070b0a9
Linus Lundahl <linus.lundahl@goodold.se>
2011-01-14 16:40:09 +0100

    Initial commit.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title><?php wp_title( '|', true, 'right' ) . bloginfo( 'name' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="content" role="main">
+<?php
+	if (have_posts()) :
+		while (have_posts()) :
+			the_post();
+			the_content();
+		endwhile;
+	endif;
+?>
+	</div><!-- #content -->
+<?php wp_footer(); ?>
+</body>
+</html>
